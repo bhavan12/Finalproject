c9cbfa1e15ecd372ec6a850af38bd196580edfc4
bhavan12 <46780437+bhavan12@users.noreply.github.com>
2019-01-21 13:00:47 +0530

    Update pytnprogram.py

--- pytnprogram.py
+++ pytnprogram.py
@@ -9,7 +9,7 @@ import pyodbc
 import pandas as pd
 import psycopg2
 import xgboost as xgb
-con = psycopg2.connect("dbname=Aizant_ML_postgre user=postgres password=Bhavancse4@ host=localhost")
+con = psycopg2.connect("dbname=dfhaphi9vtlee6 user=qcmezkrwpidutn password=3fd32869bc0d2fb997e2c47dfe339f552267e7c40bc00949638ae3b43947a64d host=ec2-107-20-183-142.compute-1.amazonaws.com")
 from flask import Flask, render_template,request
 from flask_bootstrap import Bootstrap
 from sklearn.ensemble import AdaBoostRegressor
